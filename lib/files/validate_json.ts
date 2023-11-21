a1a3365b8b6e88eb78f7e40daaa58331fe3c8688
= <bernardo.jordao@tecnico.ulisboa.pt>
2023-11-21 21:59:16 +0000

    start file pre-submission

---
+++ lib/files/validate_json.ts
@@ -0,0 +1,13 @@
+export function validateJson(json: File) {
+    
+    // TODO: make sure json is valid and has the correct fields
+
+}
+
+export function cleanJson(json: File): File {
+
+    // TODO: remove any fields that are not needed
+    var clean_json = json;
+
+    return clean_json;
+}
